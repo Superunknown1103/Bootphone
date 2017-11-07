11d2e0c1de521aaccfae6852bcbb880b8053d131
Superunknown1103 <superunknown1103@gmail.com>
2017-11-06 19:54:59 -0600

    coding challenge is complete, anything added is just for fun

--- public/main.js
+++ public/main.js
@@ -1,13 +1,27 @@
 window.onload = function() {
 
-let phonenumber = "0";
+let phoneNumber = "";
 
-
-$('.value').click(function(){
-    var number = this.attr("value", value);
+// grab the value of the digit clicked, begin formatting it.
+$('.value').on('click', function(){
+    var currentEntry = $(this).attr('value');
+    console.log(currentEntry);
+    if (phoneNumber === '') phoneNumber = currentEntry;
+    else phoneNumber = phoneNumber + currentEntry;
+    formattedPhone = '(' + phoneNumber.substr(0, 3) + ')' + '-' + phoneNumber.substr(3, 3) + '-' + phoneNumber.substr(6,4);
+    updateScreen(formattedPhone);
 })
 
+
+// update of the numbers box
+let updateScreen = function(displayValue) {
+    var displayValue = displayValue.toString();
+    $('#numbers').html(displayValue);
+};
+
+
+// initiate the call
 $('#call').click(function(){
-    alert("You are calling " + phonenumber);
+    alert("You dialed number " + formattedPhone);
 })
 }
